38d7cfedbafa585174cfc0c95b318d6d03db3224
garciajordy <jordygarcia0119@gmail.com>
2021-03-20 18:22:21 +0200

    Change solution from two loops to one loop

--- cavitymap.js
+++ cavitymap.js
@@ -2,18 +2,27 @@ function cavityMap(grid) {
     let len = grid.length
     let result = []
     result.push(grid[0])
-    for (let i = 1; i < len; i++) {
-        let row = grid[i]
-        row = row.split("")
-        for (let t = 0; t < len; t++) {
-            if ((grid[i][t] < grid[i][t + 1]) && (grid[i][t + 2] < grid[i][t + 1]) && (grid[i - 1][t + 1] < grid[i][t + 1]) && (grid[i + 1][t + 1] < grid[i][t + 1])) {
-                row[t + 1] = "X"
-
-            }
+    let rown = 1
+    let row = grid[rown]
+    for (let i = 1; rown < len-1; i++) {
+       
+        if ((grid[rown - 1][i] < grid[rown][i]) && (grid[rown][i - 1] < grid[rown][i]) && (grid[rown][i + 1] < grid[rown][i]) && (grid[rown + 1][i] < grid[rown][i])) {
+            row = row.split("")
+            row[i] = "X"
+            row = row.join("")
         }
+        
+        if (!grid[rown][i+1]) {
+            result.push(row)
+            console.log(i)
+            i = 1
+            rown++
+            row = grid[rown]
+        }
+    }
 
-        result.push(row.join(""))
+    if (len > 1) {
+        result.push(grid[len - 1])
     }
     return result
-
 }
