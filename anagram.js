76481550a69fb00bf3b8891187efbe42e9ec9945
garciajordy <jordygarcia0119@gmail.com>
2021-03-20 20:18:41 +0200

    Add more readable code

--- anagram.js
+++ anagram.js
@@ -1,33 +1,20 @@
 function anagram(s) {
-    let str1
-    let str2
-    let sum = 0
-    if (s.length % 2 == 0) {
-        str1 = s.substring(0, (s.length / 2))
-        str2 = s.substring((s.length / 2), s.length)
-
+    
+    if (s.length % 2 == 1) {
+        return -1
     }
-    else if (s.length % 2 == 1) {
-        str1 = s.substring(0, parseInt(s.length / 2))
-        str2 = s.substring(parseInt(s.length / 2), s.length)
-        sum -= 1
-        return sum
 
-    }
+    let str1 = s.substring(0, (s.length / 2)).split("")
+    let str2 = s.substring((s.length / 2), s.length).split("")
     let array = []
-    let index
-    str1 = str1.split("")
-    str2 = str2.split("")
+    let index = 0
     let len = str2.length
     for (let i = 0; i < len; i++) {
-
-        index = str1[i]
-        index = str2.indexOf(index)
+        index = str2.indexOf(str1[i])
         if (index != -1) {
             console.log(index)
             array += str2.splice(index, 1)
         }
     }
-
     return len - array.length
 }
