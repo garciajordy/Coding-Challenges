6fb29ee43e3bcc805542c90f20bf55d33c397de4
garciajordy <jordygarcia0119@gmail.com>
2021-05-19 18:19:07 +0300

    repeated string

--- repeatedString.js
+++ repeatedString.js
@@ -2,7 +2,6 @@ function repeatedString(s, n) {
     let remain = n % s.length
     let sum = 0
     if (remain != 0 && s.substring(0, remain).match(/a/gi) != null) {
-
         sum = s.substring(0, remain).match(/a/gi).length
     }
     let subStr = s.match(/a/gi)
